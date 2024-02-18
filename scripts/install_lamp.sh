d9b32135b1c47dc6d38070c0544778be6a22f36c
caguilarhernandez <144890718+caguilarhernandez@users.noreply.github.com>
2024-02-18 23:46:53 +0100

    Update install_lamp.sh

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -1,1 +1,17 @@
-
+apt update
+#apt install apache2 -y
+#systemctl restart apache2
+apt install -y mariadb-server mariadb-client
+#mariadb
+ALTER USER ‘valor2’@’localhost’ identified BY'$valor1';
+flush privileges;
+exit:
+mysql –u root –p
+create database phpmyadmin;
+use phpmyadmin;
+create user ‘$valor2’@’localhost’ identified by ‘$valor1’;
+grant all privileges on phpmyadmin.* to ‘$valor2’@’localhost’ with grant option;
+flush privileges;
+quit;
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-all-languages.zip
+unzip*.zip
